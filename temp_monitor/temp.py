42909f810683100467a0f9a52cb5d4b1cc8b774b
Ankit Raj <ankitraj.ar@gmail.com>
2018-11-09 20:16:33 +0530

    Moved Sleep inside while

--- temp_monitor/temp.py
+++ temp_monitor/temp.py
@@ -3,8 +3,9 @@ import time
 
 def measure_temp():
         temp = os.popen("vcgencmd measure_temp").readline()
-        return (temp.replace("temp=","Pi Tempearture : "))
+        return (temp.replace("temp=","Core Temperature : "))
 
 while True:
         print(measure_temp())
-time.sleep(0.5)
+        time.sleep(1)
+#Sleep outside while will cause load average to increase significantly
